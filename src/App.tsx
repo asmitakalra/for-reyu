c9037f273ac3116b9b1f7b808413b6f07f9fc4d7
Shubhankar Kalra <44618338+shubhankarkalra@users.noreply.github.com>
2026-02-08 13:56:49 +0530

    time and place

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ export default function Page() {
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOO HOOOOO!!! I love youuuuuu!</div>
+          <div className="my-4 text-4xl font-bold">WOOOOO HOOOOO!!!</div>
         </>
       ) : (
         <>
